dd937de93c454beae737ca6b9073687ce573b2a6
qiuyang1314 <1564832375@qq.com>
2021-03-13 21:58:29 +0800

    提交本地测试冲突

--- src/main/java/com/example/demo/control/addupdate.java
+++ src/main/java/com/example/demo/control/addupdate.java
@@ -3,5 +3,6 @@ package com.example.demo.control;
 public class addupdate {
     public static void main(String[] args) {
         System.out.println("d222");
+        System.out.println("123");
     }
 }
