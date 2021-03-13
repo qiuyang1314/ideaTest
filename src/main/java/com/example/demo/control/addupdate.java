fceb15420e866e25744b0b85f6acc0329259e41f
qiuyang1314 <37354023+qiuyang1314@users.noreply.github.com>
2021-03-13 21:13:39 +0800

    Update addupdate.java

--- src/main/java/com/example/demo/control/addupdate.java
+++ src/main/java/com/example/demo/control/addupdate.java
@@ -4,4 +4,7 @@ public class addupdate {
     public static void main(String[] args) {
         System.out.println("d222");
     }
+    public void method(){
+        System.out.println("kkk");
+    }
 }
