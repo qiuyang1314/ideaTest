1a8d8bc0a999145605c7e750faf1d2dfa55fff3b
qiuyang1314 <1564832375@qq.com>
2021-03-13 20:41:30 +0800

    项目初始化

---
+++ src/main/java/com/example/demo/control/addupdate.java
@@ -0,0 +1,7 @@
+package com.example.demo.control;
+
+public class addupdate {
+    public static void main(String[] args) {
+        System.out.println("d222");
+    }
+}
---
+++ src/main/java/com/example/demo/control/control.java
@@ -0,0 +1,32 @@
+package com.example.demo.control;
+
+
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AutoConfigurationPackage
+@RequestMapping("/hello")
+public class control {
+
+    @RequestMapping(value="/sayhello")
+    public String index() {
+        System.out.println("23454");
+        return "Hello World";
+    }
+    public void method() {
+        System.out.println("drg");
+        System.out.println("drg123");
+    }
+
+    public void metho() {
+
+        System.out.println("123");
+    }
+
+    public void add() {
+
+        System.out.println("1aaa");
+    }
+}
